36e905a24c8a0632aac310e8fb1739f26635cb83
Cristian Maglie <c.maglie@bug.st>
2023-02-24 12:47:36 +0100

    Import protocol handler

--- main.go
+++ main.go
@@ -10,6 +10,41 @@ package main
 */
 import "C"
 
+import (
+	"fmt"
+	"os"
+
+	discovery "github.com/arduino/pluggable-discovery-protocol-handler/v2"
+)
+
 func main() {
-	C.probe_devices(nil)
+	dfuDisc := &DFUDiscovery{}
+	disc := discovery.NewServer(dfuDisc)
+	if err := disc.Run(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+// DFUDiscovery is the implementation of the DFU pluggable-discovery
+type DFUDiscovery struct {
+}
+
+// Hello is the handler for the pluggable-discovery HELLO command
+func (d *DFUDiscovery) Hello(userAgent string, protocolVersion int) error {
+	return nil
+}
+
+// Quit is the handler for the pluggable-discovery QUIT command
+func (d *DFUDiscovery) Quit() {
+}
+
+// Stop is the handler for the pluggable-discovery STOP command
+func (d *DFUDiscovery) Stop() error {
+	return nil
+}
+
+// StartSync is the handler for the pluggable-discovery START_SYNC command
+func (d *DFUDiscovery) StartSync(eventCB discovery.EventCallback, errorCB discovery.ErrorCallback) error {
+	return nil
 }
