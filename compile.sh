ea11018566ab73fe15a6e7ef673a6279c17dfc34
Cristian Maglie <c.maglie@bug.st>
2023-02-24 11:25:47 +0100

    Rebased dfu-discovery source on dfu-util-0.11 (#6)
    The source code of dfu-util is now downloaded from the origin.
    The build for mac is not yer done.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,17 @@
-g++ main.cpp -I/usr/include/libusb-1.0 -lusb-1.0 -fpermissive -o discoverer
+DFU_UTIL=dfu-util-0.11
+
+if [ -d "$DFU_UTIL" ];
+then
+	echo "$DFU_UTIL already downloaded (directory exists)."
+else
+	echo "Downloading $DFU_UTIL..."
+	curl https://dfu-util.sourceforge.net/releases/$DFU_UTIL.tar.gz -s | tar xz
+fi
+
+DEFINES="-DHAVE_UNISTD_H -DHAVE_NANOSLEEP"
+INCLUDES="-I. -Idfu-util-0.11/src -I/usr/include/libusb-1.0"
+gcc -c dfu-util-0.11/src/dfuse_mem.c $INCLUDES $DEFINES
+gcc -c dfu-util-0.11/src/dfu_util.c  $INCLUDES $DEFINES
+gcc -c dfu-util-0.11/src/quirks.c    $INCLUDES $DEFINES
+g++ -c main.cpp                      $INCLUDES $DEFINES
+g++ dfu_util.o quirks.o dfuse_mem.o main.o -lusb-1.0 -o dfu-discovery
