7c99c4a428216f9ce0a05ccace821d090696a5d9
Cristian Maglie <c.maglie@arduino.cc>
2023-02-28 16:59:23 +0100

    Some refinements in source tree builder

--- compile.sh
+++ compile.sh
@@ -7,7 +7,9 @@ else
 	echo "Downloading $DFU_UTIL..."
 	curl https://dfu-util.sourceforge.net/releases/$DFU_UTIL.tar.gz -s | tar xz
 
-	cp dfu-util-0.11/src/dfuse_mem.c .
-	cp dfu-util-0.11/src/dfu_util.c .
-	cp dfu-util-0.11/src/quirks.c .
+	cp dfu-util-0.11/src/dfuse_mem.c dfu-util_dfuse_mem.c
+	cp dfu-util-0.11/src/dfu_util.c dfu-util_dfu_util.c
+	cp dfu-util-0.11/src/quirks.c dfu-util_quirks.c
+	# for some reason quirks.c has the exec flag set
+	chmod -x dfu-util_quirks.c
 fi
