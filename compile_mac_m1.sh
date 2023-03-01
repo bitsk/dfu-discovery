e1ec0089eb32130ee858aed3bd81701cb1e3786a
Umberto Baldi <u.baldi@arduino.cc>
2023-03-01 10:34:46 +0100

    add task to perform the download of dfu-util and remove the old bash scripts

--- compile_mac_m1.sh
+++
@@ -1,5 +0,0 @@
-# for Mac OS 12.6.3 Apple Silicon M1 Max
-# libusb-1.0 installed with homebrew
-# then
-# sudo ln -s /opt/homebrew/lib/libusb-1.0.dylib /usr/local/lib/libusb.dylib
-g++ main.cpp -I/opt/homebrew/Cellar/libusb/1.0.26/include/libusb-1.0 -lusb -fpermissive -std=c++14 -o dfu_discoverer -v
